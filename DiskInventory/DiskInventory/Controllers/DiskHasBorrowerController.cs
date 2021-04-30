458bd8019842a8dd4caecbe19cd7335bdac63285
Adrian Arias <63442432+adrian-arias@users.noreply.github.com>
2021-04-30 16:40:22 -0600

    Added borrower page to check out discs. Added edit function with it.

---
+++ DiskInventory/DiskInventory/Controllers/DiskHasBorrowerController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DiskInventory.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiskInventory.Controllers
+{
+    public class DiskHasBorrowerController : Controller
+    {
+        private disk_inventoryAAContext context { get; set; }
+        public DiskHasBorrowerController (disk_inventoryAAContext ctx)
+        {
+            context = ctx;
+        }
+        public IActionResult Index()
+        {
+            var diskhasborrowers = context.DiscHasBorrowers.
+            Include(d => d.Disc).OrderBy(d => d.Disc.DiscName).
+            Include(b => b.Borrower).ToList();
+            return View(diskhasborrowers);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Add";
+            ViewBag.Discs = context.Discs.OrderBy(d => d.DiscName).ToList();
+            ViewBag.Borrowers = context.Borrowers.OrderBy(b => b.Lname).ToList();
+            DiscHasBorrower newdiskhasborrower = new DiscHasBorrower();
+            newdiskhasborrower.BorrowedDate = DateTime.Today;
+            newdiskhasborrower.DueDate = DateTime.Today; 
+            return View("Edit", newdiskhasborrower);
+            
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Action = "Edit";
+            ViewBag.Discs = context.Discs.OrderBy(d => d.DiscName).ToList();
+            ViewBag.Borrowers = context.Borrowers.OrderBy(b => b.Lname).ToList();
+            var diskhasborrower = context.DiscHasBorrowers.Find(id);
+            return View(diskhasborrower);
+            
+        }
+        [HttpPost]
+        public IActionResult Edit(DiscHasBorrower discHasBorrower)
+        {
+            if (ModelState.IsValid)
+            {
+                if (discHasBorrower.DiscHasBorrowerId == 0)
+                {
+                    context.DiscHasBorrowers.Add(discHasBorrower);
+                }
+                else
+                {
+                    context.DiscHasBorrowers.Update(discHasBorrower);
+                }
+                context.SaveChanges();
+                return RedirectToAction("Index", "DiskHasBorrower");
+            }
+            else
+            {
+                ViewBag.Action = (discHasBorrower.DiscHasBorrowerId == 0) ? "Add" : "Edit";
+                ViewBag.Discs = context.Discs.OrderBy(d => d.DiscName).ToList();
+                ViewBag.Borrowers = context.Borrowers.OrderBy(b => b.Lname).ToList();
+               
+                return View(discHasBorrower);
+            }          
+        }
+    }
+}
--- DiskInventory/DiskInventory/Models/DiscHasBorrower.cs
+++ DiskInventory/DiskInventory/Models/DiscHasBorrower.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -8,10 +9,14 @@ namespace DiskInventory.Models
     public partial class DiscHasBorrower
     {
         public int DiscHasBorrowerId { get; set; }
+        [Required(ErrorMessage ="Please enter a borrowed date.")]
         public DateTime BorrowedDate { get; set; }
+        [Required(ErrorMessage = "Please enter a due date.")]
         public DateTime DueDate { get; set; }
         public DateTime? ReturnedDate { get; set; }
+        [Required(ErrorMessage = "Please enter a borrower.")]
         public int BorrowerId { get; set; }
+        [Required(ErrorMessage = "Please select a disc.")]
         public int DiscId { get; set; }
 
         public virtual Borrower Borrower { get; set; }
